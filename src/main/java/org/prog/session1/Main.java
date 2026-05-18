1d08b7de64acef43dcf9861955f30006fe1e53ae
aosipenko <aoskiev@gmail.com>
2026-05-18 19:55:20 +0200

    Session 1

---
+++ src/main/java/org/prog/session1/Main.java
@@ -0,0 +1,45 @@
+package org.prog.session1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        // here is explain what variables are
+        int i = 10;
+        int j = 20;
+        int k = 30;
+        int amount = 1000;
+
+        System.out.println(i);
+        System.out.println(amount);
+
+        System.out.println(i + j);
+        System.out.println(k - amount);
+        System.out.println(i * j);
+        System.out.println(amount / j);
+
+        boolean b = true;
+        char c = 167;
+        byte bt = 127;
+        short sh = 32767;
+        int i1 = 123;
+        long l1 = 1234567890L;
+        float f = 123.123F;
+        double d = 123.123D;
+        //0 -> 0
+        //1 -> 1
+        //2 -> 10
+        //3 -> 11
+        //4 -> 100
+        //5 -> 101
+        //6 -> 110
+        //7 -> 111
+        //8 -> 1000
+        //9 -> 1001
+        //10
+        // 11111111 -> -128
+
+
+        System.out.println(c);
+    }
+}
